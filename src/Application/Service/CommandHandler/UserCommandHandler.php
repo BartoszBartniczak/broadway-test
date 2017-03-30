36d3178478bdbd83a79b08c9c11b2479639bcc61
Bartosz Bartniczak <bartoszbartniczak@gmail.com>
2017-03-30 22:02:00 +0200

    UserCommandHandler

---
+++ src/Application/Service/CommandHandler/UserCommandHandler.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bartosz Bartniczak <kontakt@bartoszbartniczak.pl>
+ */
+
+namespace BartoszBartniczak\Demo\Application\Service\CommandHandler;
+
+
+use BartoszBartniczak\Demo\Domain\Model\User\Command\CommandHandler;
+use BartoszBartniczak\Demo\Domain\Model\User\Command\CreateNewUserCommand;
+use Broadway\CommandHandling\SimpleCommandHandler;
+
+class UserCommandHandler extends SimpleCommandHandler
+{
+
+    /**
+     * @var CommandHandler
+     */
+    private $commandHandler;
+
+    /**
+     * UserCommandHandler constructor.
+     * @param CommandHandler $commandHandler
+     */
+    public function __construct(CommandHandler $commandHandler)
+    {
+        $this->commandHandler = $commandHandler;
+    }
+
+    public function handleCreateNewUser(CreateNewUserCommand $createNewUserCommand)
+    {
+        return $this->commandHandler->handleCreateNewUser($createNewUserCommand);
+    }
+
+}
